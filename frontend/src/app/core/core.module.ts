ee933054cd0b6588dba7fec4e069c650da2e894d
Tatiane Navarro <tatianenavarro@pecege.com>
2020-01-27 11:06:45 -0300

    Aula 3 completa

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,7 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
   declarations: [ HeaderComponent ],
   exports: [ HeaderComponent ],
 })
--- frontend/src/app/core/user/user.service.ts
+++ frontend/src/app/core/user/user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import * as jwt_decode from 'jwt-decode';
 import { TokenService } from './../token/token.service';
 import { User } from './user';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-  private userSubject = new Subject<User>();
+  private userSubject = new BehaviorSubject<User>(null);
+  private userName: string;
 
   constructor(private tokenService: TokenService) {
     this.tokenService.hasToken() &&
@@ -16,8 +17,6 @@ export class UserService {
   setToken(token: string) {
     this.tokenService.setToken(token);
     this.decodeAndNotify();
-    jwt_decode.decode(token);
-    this.userSubject.next();
   }
 
   getUser() {
@@ -27,6 +26,20 @@ export class UserService {
   private decodeAndNotify() {
     const token = this.tokenService.getToken();
     const user = jwt_decode(token) as User;
+    this.userName = user.name;
     this.userSubject.next(user);
+  }
+
+  logout() {
+    this.tokenService.removeToken();
+    this.userSubject.next(null);
+  }
+
+  isLogged() {
+    return this.tokenService.hasToken();
+  }
+
+  getUserName() {
+    return this.userName;
   }
 }
