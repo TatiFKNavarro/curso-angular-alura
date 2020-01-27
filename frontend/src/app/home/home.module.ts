ebd1a7f5db8e854491da82e39258eefc28b9d66b
Tatiane Navarro <tatianenavarro@pecege.com>
2020-01-27 15:40:04 -0300

    Aula 4

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { SigninComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { VMessageModule } from './../shared/components/vmessage/vmessage.module';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [ SigninComponent ],
+  declarations: [
+    SigninComponent,
+    SignupComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
+    RouterModule,
     ReactiveFormsModule,
     VMessageModule
   ],
