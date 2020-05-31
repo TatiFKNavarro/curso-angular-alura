d1c0e98095754f72b2c2557a3e60a3fd3444111a
Tati Navarro <tatinavarro@outlook.com.br>
2020-05-30 21:46:03 -0300

    Aula 4 concluída

--- frontend/src/app/photos/photo-list/photo-list.component.ts
+++ frontend/src/app/photos/photo-list/photo-list.component.ts
@@ -1,36 +1,39 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
-import { Photo } from '../photo/photo';
-import { PhotoService } from './../photo/photo.service';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subject } from "rxjs";
+import { debounceTime } from "rxjs/operators";
+import { Photo } from "../photo/photo";
+import { PhotoService } from "./../photo/photo.service";
 
 @Component({
-  selector: 'ap-photo-list',
-  templateUrl: './photo-list.component.html',
-  styleUrls: ['./photo-list.component.css']
+  selector: "ap-photo-list",
+  templateUrl: "./photo-list.component.html",
+  styleUrls: ["./photo-list.component.css"],
 })
 export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
-  filter: string = '';
+  filter: string = "";
   hasMore: boolean = true;
   currentPage: number = 1;
-  userName: string = '';
+  userName: string = "";
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private photoService: PhotoService,
-  ) { }
+    private photoService: PhotoService
+  ) {}
 
   ngOnInit(): void {
-    this.userName = this.activatedRoute.snapshot.params.userName;
-    this.photos = this.activatedRoute.snapshot.data.photos;
+    this.activatedRoute.params.subscribe((params) => {
+      this.userName = params.username;
+      this.photos = this.activatedRoute.snapshot.data.photos;
+    });
   }
 
   load() {
-    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
       .subscribe((photos) => {
-        this.filter = '';
+        this.filter = "";
         this.photos = this.photos.concat(...photos);
         if (!photos.length) {
           this.hasMore = false;
