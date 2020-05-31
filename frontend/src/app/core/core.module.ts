8bbdb579e7187d1c4eaa246147eff8c1ed12aa8c
Tati Navarro <tatinavarro@outlook.com.br>
2020-05-31 13:47:24 -0300

    Aula 6 concluída

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,30 +1,22 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RequestInterceptor } from './auth/request.interceptor';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { RequestInterceptor } from "./auth/request.interceptor";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { AlertModule } from "../shared/alert/alert.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-  ],
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-  ],
-  exports: [
-    HeaderComponent,
-    FooterComponent,
-  ],
+  imports: [CommonModule, RouterModule, AlertModule],
+  declarations: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: RequestInterceptor,
       multi: true,
-    }
-  ]
+    },
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
