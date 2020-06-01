609d4b5250dea51cd8b1dfdd3fb3b77b34d2bea5
Tatiane Navarro <tatianenavarro@pecege.com>
2020-06-01 15:42:50 -0300

    Curso 3 concluído

--- frontend/src/app/photos/photo/photo.service.ts
+++ frontend/src/app/photos/photo/photo.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { of, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 import { Photo } from "./photo";
 import { PhotoComment } from "./photo-comment";
-import { map, catchError } from "rxjs/operators";
-import { of, throwError } from "rxjs";
+import { environment } from "src/environments/environment";
 
-const API = "http://localhost:3000";
+const API = environment.ApiUrl;
 
 @Injectable({ providedIn: "root" })
 export class PhotoService {
